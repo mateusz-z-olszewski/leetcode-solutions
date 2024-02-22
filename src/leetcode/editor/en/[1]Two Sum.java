c89a18ae2dea02e9067b1bb271d65e549a415f15
Mateusz Olszewski <m.z.olszewski@student.tudelft.nl>
2024-02-22 03:02:19 +0100

    Solved 1 - "Two Sum" - 98.2% time, 34.9% space

---
+++ src/leetcode/editor/en/[1]Two Sum.java
@@ -0,0 +1,23 @@
+package leetcode.editor.en;
+
+import java.util.*;
+
+public class TwoSum {
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> seenAtIndex = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int remainder = target - nums[i];
+            if(seenAtIndex.containsKey(remainder))
+                return new int[]{seenAtIndex.get(remainder), i};
+            seenAtIndex.put(nums[i], i);
+        }
+        throw new RuntimeException(); // never gets here.
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+
+}
