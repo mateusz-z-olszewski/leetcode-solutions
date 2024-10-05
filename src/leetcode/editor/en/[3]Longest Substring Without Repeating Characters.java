8ee479dfe954ef88bcccda1cef28697206227f35
Mateusz Olszewski <matix2k4@outlook.com>
2024-10-05 22:25:03 +0200

    Solved 3 - "Longest Substring Without Repeating Characters" - 100% time, 88% space

---
+++ src/leetcode/editor/en/[3]Longest Substring Without Repeating Characters.java
@@ -0,0 +1,27 @@
+package leetcode.editor.en;
+
+
+class LongestSubstringWithoutRepeatingCharacters {
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public int lengthOfLongestSubstring(String s) {
+        int[] seenAt = new int[128];
+        int l = 0, max = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            int r = i - seenAt[c] + 1;
+            seenAt[c] = i + 1;
+
+            l = Math.min(r, l + 1);
+            max = Math.max(max, l);
+        }
+
+        return max;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+
+}
