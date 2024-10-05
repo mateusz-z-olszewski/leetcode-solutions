58f2bf3d0ba5c9967ad17c77aee6dec65fbf4989
Mateusz Olszewski <matix2k4@outlook.com>
2024-10-05 20:34:11 +0200

    Solved 2 - "Add Two Numbers" - 100% time, 42% space

---
+++ src/leetcode/editor/en/[2]Add Two Numbers.java
@@ -0,0 +1,49 @@
+package leetcode.editor.en;
+
+import java.util.*;
+
+class AddTwoNumbers {
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        int digit = l1.val + l2.val;
+        ListNode out = new ListNode(digit % 10), current = out;
+        l1 = l1.next;
+        l2 = l2.next;
+        int carry = digit >= 10 ? 1 : 0;
+        while(l1 != null || l2 != null || carry != 0) {
+            int x1 = l1 != null ? l1.val : 0;
+            int x2 = l2 != null ? l2.val : 0;
+            int sum = x1 + x2 + carry;
+            carry = sum >= 10 ? 1 : 0;
+            current.next = new ListNode(sum % 10);
+            l1 = l1 != null ? l1.next : null;
+            l2 = l2 != null ? l2.next : null;
+            current = current.next;
+        }
+        return out;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+
+}
+
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode() {}
+    ListNode(int val) { this.val = val; }
+    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+}
