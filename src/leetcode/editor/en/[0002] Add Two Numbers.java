8d77748533c0424dd4385b0d21ac33f6471ca8a9
Mateusz Olszewski <matix2k4@outlook.com>
2024-10-21 18:45:03 +0200

    Solved 2

--- src/leetcode/editor/en/[0002] Add Two Numbers.java
+++ src/leetcode/editor/en/[0002] Add Two Numbers.java
@@ -36,14 +36,12 @@ class Solution {
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
-
-
-}
-
 class ListNode {
     int val;
     ListNode next;
     ListNode() {}
     ListNode(int val) { this.val = val; }
     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+}
+
 }
