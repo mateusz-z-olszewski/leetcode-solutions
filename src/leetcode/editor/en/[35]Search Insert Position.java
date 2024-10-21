efc96df736d0b80ad9cb0219f2467fe748c355ee
Mateusz Olszewski <matix2k4@outlook.com>
2024-10-21 16:26:29 +0200

    Solved 35

---
+++ src/leetcode/editor/en/[35]Search Insert Position.java
@@ -0,0 +1,29 @@
+package leetcode.editor.en;
+
+import java.util.*;
+
+class SearchInsertPosition {
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        return searchRecursive(nums, 0, nums.length, target);
+    }
+
+    final int LINEAR_CUTOFF = 4;
+    private int searchRecursive(int[] nums, int low, int high, int target){
+        if(high - low <= LINEAR_CUTOFF){
+            for (int i = low; i < high; i++)
+                if (target <= nums[i]) return i;
+            return high;
+        }
+
+        int midpoint = (high + low) / 2;
+        if(target < nums[midpoint]) return searchRecursive(nums, low, midpoint, target);
+        else return searchRecursive(nums, midpoint, high, target);
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+
+}
