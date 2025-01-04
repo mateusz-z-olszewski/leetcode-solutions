1c9a0f132d4fddfe671dacc4a21c222fbc43e559
Mateusz Olszewski <matix2k4@outlook.com>
2025-01-04 02:47:11 +0100

    Solved 100

---
+++ src/leetcode/editor/en/[0100] Same Tree.java
@@ -0,0 +1,47 @@
+package leetcode.editor.en;
+
+import java.util.*;
+
+class SameTree {
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if(p == null || q == null) return p == q;
+        return p.val == q.val
+                && isSameTree(p.left, q.left)
+                && isSameTree(p.right, q.right);
+    }
+
+
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode() {}
+        TreeNode(int val) { this.val = val; }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
